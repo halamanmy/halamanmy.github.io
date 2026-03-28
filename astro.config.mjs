374eee4ecb9c406a746542da2cb5d02c3ba73060
naquib99 <a.naquib5@gmail.com>
2026-03-29 00:08:26 +0800

    Update 1. Rename Blog to Posts 2. Modify a bit on the Navigation bar

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,6 +11,7 @@ import svelte from '@astrojs/svelte';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.ruangan.my',
+  base: '/',
   integrations: [mdx(), sitemap(), svelte()],
 
   vite: {
